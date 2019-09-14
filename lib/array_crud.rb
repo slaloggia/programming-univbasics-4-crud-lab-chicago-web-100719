919e66e26860bd2143f3ba4200dd214cf55be376
Sarah Laloggia <sarah.laloggia@gmail.com>
2019-09-14 18:05:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  favorite_colors << "teal"
+  create_an_array << "teal"
 end
 
 def add_element_to_start_of_array(array, element)
